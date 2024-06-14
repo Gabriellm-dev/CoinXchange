00850877178ccb92e23bc0f4a5abe5efac532981
gabriellm.dev <gabriellm.dev@gmail.com>
2024-06-14 09:53:27 -0300

    created model currency

--- src/br/com/coinXchange/model/Currency.java
+++ src/br/com/coinXchange/model/Currency.java
@@ -1,4 +1,38 @@
 package br.com.coinXchange.model;
 
 public class Currency {
+    private String code;
+    private String name;
+
+    // Construtor
+    public Currency(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // Métodos getters e setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Método toString para representação textual da moeda
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
